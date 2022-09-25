b5044d481a44288cf9ec81ad150c78d5033f1cc4
Abrar <saquib.misles@gmail.com>
2022-09-25 20:04:46 +0530

    Add files via upload

---
+++ php/info.php
@@ -0,0 +1,26 @@
+<?php 
+// Website Name
+$webName = "AniKatsu";
+// Website URL
+$webUrl = "//{$_SERVER['SERVER_NAME']}";  //for example $webUrl = "//{$_SERVER['SERVER_NAME']}:8888";
+// Logo
+$webLogo = "/files/images/logo.png";
+
+// Socials 
+// 1 . Donate 
+$donate = "https://r.honeygain.me/DEVEL77939";
+// 2 . Telegram
+$telegram = "https://t.me/Anikatsu";
+// 3 . Discord 
+$discord = "https://discord.com/invite/FrWnWRrZmk";
+// 4 Redit
+$redit = "#";
+// Twitter
+$twitter = "#";
+
+
+
+// API URL (Don't edit)
+$apiLink = "https://anikatsu.vercel.app/api";
+$banner = $webUrl . "/files/images/banner.png"
+?>
